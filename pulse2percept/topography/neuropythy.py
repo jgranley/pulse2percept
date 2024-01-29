ab840b208f2786ddf31007f02d3b8d77798f47f0
jgranley <jgranley@ucsb.edu>
2024-01-29 11:20:30 -0800

    add neuropythy error ;

--- pulse2percept/topography/neuropythy.py
+++ pulse2percept/topography/neuropythy.py
@@ -9,7 +9,8 @@ class NeuropythyMap(CorticalMap):
 
     def __init__(self, subject, cache_dir=None, **params):
         """
-        Uses the visual field maps from the neuropythy package
+        Uses the visual field maps from the neuropythy package.
+        Requires neuropythy to be installed (`pip install neuropythy`)
 
         Parameters
         ----------
@@ -39,7 +40,7 @@ class NeuropythyMap(CorticalMap):
         try:
             import neuropythy as ny
         except ImportError:
-            raise ImportError("NeuropythyMap requires the neuropythy package.")
+            raise ImportError("NeuropythyMap requires the neuropythy package (`pip install neuropythy`)")
         super().__init__(**params)
         self.cache_dir = os.path.expanduser(os.path.join('~', '.neuropythy_p2p')) if cache_dir is None else os.path.expanduser(cache_dir)
         self.subject = self.parse_subject(subject)
